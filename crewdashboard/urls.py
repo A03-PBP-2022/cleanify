ade6e3707bd3750e150a2496b88c28f104ffb985
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-10-28 14:42:17 +0700

    Location Form

--- crewdashboard/urls.py
+++ crewdashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from crewdashboard import dashboard
+from crewdashboard.views import add_new_locations, show_locations, show_json
 
 app_name = 'crewdashboard'
 
 urlpatterns = [
     path('dashboard/', show_locations, name='show_locations'),
+    path('json/', show_json, name='json'),
+    path('addlocation/', add_new_locations, name='addlocation'),
 ]
--- crewdashboard/views.py
+++ crewdashboard/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.core import serializers
+from .models import Locations
+import datetime
 
 # Create your views here.
 
-@login_required(login_url='/cleanify/login/')
-def show_dashboard(request):
-    return render(request, "todolist_ajax.html")
+def show_locations(request):
+    return render(request, "dashboard.html")
 
 def show_json(request):
-    user = request.user
-    data_todo_list = ListToDo.objects.filter(user=user)
-    return HttpResponse(serializers.serialize("json", data_todo_list), content_type="application/json")
+    data = Locations.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def add_new_locations(request):
+    if request.method == 'POST':
+        location = request.POST.get('location')
+        image = request.POST.get('description')
+        urgency = request.POST.get('urgency')
+        description = request.POST.get('description')
+
+        new_location = Locations.objects.create(
+            date = datetime.datetime.now(),
+            location = location,
+            image = image,
+            urgency = urgency,
+            description = description,
+            )
+        new_location.save()
+        return HttpResponse("")
+    return render(request, 'locations.html')
