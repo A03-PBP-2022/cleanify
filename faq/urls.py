5b40183da5315cef9ae55537fb42492e9a770b49
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-10-30 19:40:55 +0700

    FAQ: Ganti URL

--- faq/urls.py
+++ faq/urls.py
@@ -4,7 +4,7 @@ from .views import faq_index, faq_json, faq_add, faq_update_thumbsUp
 app_name = 'faq'
 urlpatterns = [
     path('', faq_index, name='faq_index'),
-    path('faq_json/', faq_json, name='faq_json'),
-    path('faq_add/', faq_add, name='faq_add'),
-    path('faq_update_thumbsUp/', faq_update_thumbsUp, name='faq_update_thumbsUp')
+    path('json/', faq_json, name='faq_json'),
+    path('add/', faq_add, name='faq_add'),
+    path('thumbsup', faq_update_thumbsUp, name='faq_update_thumbsUp')
 ]
