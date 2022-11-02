388907a20ea888fbf376e2b6f890ef3fd1b60754
Thalia Fortuna <thaliafortuna8@gmail.com>
2022-11-02 18:07:35 +0700

    Cards Modif

--- static/js/report/dashboard.js
+++ static/js/report/dashboard.js
@@ -1,5 +1,5 @@
 counter = 0
-function updateTable() {
+function updateCards() {
 	var tempCounter = counter;
 	// AJAX GET melakukan append data
 	$.get(window.JSON_URL, function (data) {
@@ -8,21 +8,21 @@ function updateTable() {
 		let string = ``
 		data.forEach(report => {
 			string += `
-			<div class="card-group">
-			<div class = "card mb-2 bg-light">
-				<div class="card-header">
-					<small class = "card-date">${report.fields.date}</small>
-				</div>
-				<div class="card-body">
-					<small class="card-title">Urgency level: ${report.fields.urgency}</small>
-					<div>
-						<small class="card-description">Description: ${report.fields.description}</small>
+			<div class="card-group" >
+				<div class = "card mb-2 bg-light">
+					<div class="card-header">
+						<small class = "card-date">${report.fields.date}</small>
+					</div>
+					<div class="card-body">
+						<small class="card-title">Urgency level: ${report.fields.urgency}</small>
+						<div>
+							<small class="card-description">Description: ${report.fields.description}</small>
+						</div>
+					</div>
+					<div class="card-footer">
+						<small class="card-description">${report.fields.location}</small>
 					</div>
 				</div>
-				<div class="card-footer">
-					<small class="card-description">${report.fields.location}</small>
-				</div>
-			</div>
 			</div>`
 			tempCounter++;
 			
@@ -33,5 +33,5 @@ function updateTable() {
 }
 
 $(document).ready(function () {
-	updateTable();
+	updateCards();
 })
