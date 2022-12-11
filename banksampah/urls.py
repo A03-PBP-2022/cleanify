3c415fa5a3ec278894c108e441da947c821660b3
rafiadiwibowo <muhammad.rafi15@ui.ac.id>
2022-12-11 14:12:21 +0700

    update change flutter

--- banksampah/urls.py
+++ banksampah/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('contents/', show_bank, name='show'),
     path('delete/<int:id>/', delete_bank, name='delete'),
     path('json/', show_banksampah_json, name='show_json'),
-    path('createbank_flutter', flutter_createbank, name='createbankflutter')
+    path('createbank_flutter/', flutter_createbank, name='createbankflutter')
 ]
