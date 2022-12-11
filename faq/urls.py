fa6a83e2c3a6676b46431d31a7afff2c6afd6f77
alifilhmm <alifilhmm@gmail.com>
2022-12-11 14:31:48 +0700

    update flutter faq

--- faq/urls.py
+++ faq/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, json, add, update_thumbsUp
+from .views import index, json, add, update_thumbsUp,  Add_from_flutter
 
 app_name = 'faq'
 urlpatterns = [
     path('', index, name='index'),
     path('json/', json, name='json'),
     path('add/', add, name='add'),
-    path('thumbsup/', update_thumbsUp, name='update_thumbsUp')
+    path('thumbsup/', update_thumbsUp, name='update_thumbsUp'),
+    path('addFlutter/', Add_from_flutter, name ='Add_from_flutter')
+
 ]
